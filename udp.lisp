cbe8a86e58dd161b570b5961094e01b6af5dcb02
Ben Hyde <bhyde@pobox.com>
2013-02-06 17:02:34 -0500

    The 2nd commit should always be whitespace cleanup!  It's a tradition.

--- udp.lisp
+++ udp.lisp
@@ -2,33 +2,34 @@
 
 ;;;; A simple UDP send/recieve for Lispworks
 
-;; Evolved from Marc Battyani's original: http://comments.gmane.org/gmane.lisp.lispworks.general/6643
+;; Evolved from Marc Battyani's original:
+;;     http://comments.gmane.org/gmane.lisp.lispworks.general/6643
 
 (fli:define-c-typedef (socket (:foreign-name "SOCKET"))
                       (:unsigned :int))
- 
+
 (fli:define-c-struct (in-addr (:foreign-name "in_addr"))
                      (s-addr (:unsigned :long)))
- 
+
 (fli:define-c-struct (sockaddr-in (:foreign-name "sockaddr_in"))
                      (sin-family :short)
                      (sin-port (:unsigned :short))
                      ;(sin-addr (:unsigned :long))
                      (sin-addr :uint32)
                      (sin-zero (:c-array :char 8)))
- 
+
 (fli:define-c-typedef udp-buffer (:c-array (:unsigned :char) 64))
- 
+
 (fli:define-foreign-function (socket "socket" :source)
                              ((af :int) (type :int) (protocol :int))
                              :result-type socket
                              :language :ansi-c
                              :calling-convention :cdecl)
- 
+
 (fli:Define-c-struct (sockaddr
                       (:foreign-name "sockaddr")
                       (:forward-reference t)))
- 
+
 (fli:define-foreign-function (bind "bind" :source)
                              ((s socket)
                               (name (:pointer (:struct sockaddr-in)))
@@ -36,7 +37,7 @@
                              :result-type :int
                              :language :ansi-c
                              :calling-convention :cdecl)
- 
+
 (fli:define-foreign-function (set-sock-opt "setsockopt" :source)
                              ((s socket)
                               (level :int)
@@ -46,7 +47,7 @@
                              :result-type :int
                              :language :ansi-c
                              :calling-convention :cdecl)
- 
+
 (fli:define-foreign-function (send-to "sendto" :source)
                              ((s socket)
                               ; (buf (:pointer :char))
@@ -58,7 +59,7 @@
                              :result-type :int
                              :language :ansi-c
                              :calling-convention :cdecl)
- 
+
 (fli:define-foreign-function (recv-from "recvfrom" :source)
                              ((s socket)
                               (buf :pointer)
@@ -69,20 +70,20 @@
                              :result-type :int
                              :language :ansi-c
                              :calling-convention :cdecl)
- 
+
 (fli:define-foreign-function (close-socket "closesocket" :source)
                              ((s socket))
                              :result-type :int
                              :language :ansi-c
                              :calling-convention :cdecl)
- 
+
 (defconstant AF_INET           2)
 (defconstant SOCK_DGRAM        2)
 (defconstant IPPROTO_UDP      17)
 (defconstant INADDR_ANY  #x00000000)
 (defconstant IPPROTO_IP        0)
 (defconstant IP_MULTICAST_TTL 33)
- 
+
 
 ;;;; A UDP listener.
 
@@ -93,7 +94,7 @@
 
 (defvar *udp-socket* nil)
 (defvar *udp-buffer* nil)
- 
+
 (defun start-udp-listener ()
   (unless *udp-socket*
     (setf *udp-socket* (socket AF_INET SOCK_DGRAM IPPROTO_UDP))
@@ -105,7 +106,7 @@
               (fli:foreign-slot-value recv-addr 'sin-addr) INADDR_ANY)
         (bind *udp-socket* recv-addr (fli:size-of 'sockaddr-in)))))
   *udp-socket*)
- 
+
 (defun read-udp-socket ()
   (when *udp-socket*
     (fli:with-dynamic-foreign-objects ()
@@ -117,7 +118,7 @@
          (loop for i from 0 below nb-read collect (fli:foreign-aref *udp-buffer* i))
          (fli:foreign-slot-value sender-addr 'sin-port)
          (fli:foreign-slot-value sender-addr 'sin-addr))))))
- 
+
 (defun stop-udp-listener ()
   (when *udp-socket*
     (close-socket *udp-socket*)
@@ -153,14 +154,16 @@
      *udp-send-sockaddr_in* (fli:allocate-foreign-object :type 'sockaddr-in)
      (fli:foreign-slot-value *udp-send-sockaddr_in* 'sin-family) AF_INET
           (fli:foreign-slot-value *udp-send-sockaddr_in* 'sin-addr) (htonl ip)
-          (fli:foreign-slot-value *udp-send-sockaddr_in* 'sin-port) (htons port))
+          (fli:foreign-slot-value *udp-send-sockaddr_in* 'sin-port)
+          (htons port))
     (setf *udp-sending-socket*
           (setf *udp-socket* (socket AF_INET SOCK_DGRAM IPPROTO_UDP))))
 
 
 (defun send-udp (buffer start end)
   (replace *udp-buffer-pad* buffer :start2 start :end2 end)
-  (fli:with-dynamic-lisp-array-pointer (ptr *udp-buffer-pad* :type :unsigned-byte)
+  (fli:with-dynamic-lisp-array-pointer
+      (ptr *udp-buffer-pad* :type :unsigned-byte)
     (send-to *udp-sending-socket* ptr (- end start)
              0 ; flags
              (fli:copy-pointer *udp-send-sockaddr_in* :type '(:struct sockaddr))
